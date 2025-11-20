7f5855d4fd7886398857c2bdb8fc19b69cf4e16f
annevm1 <annevmill@gmail.com>
2025-11-20 12:56:39 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-console.log("hi");
-
-const hamburger = document.getElementById('hamburger');
-const navigatie = document.getElementById('navigatie');
-
-hamburger.addEventListener('click', () => {
-  navigatie.classList.toggle('active');
-});
