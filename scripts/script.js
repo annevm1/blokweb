14be448c90b81cd8b57f7cb73239ec2b1587c6db
annevm1 <annevmill@gmail.com>
2026-01-20 16:01:45 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,14 @@
-// JavaScript Document
-console.log("hi");
+
+const menuButton = document.querySelector('.hamburger');
+const nav = document.querySelector('.menu');
+const sluitButton = document.querySelector('.sluit');
+
+menuButton.addEventListener('click', () => {
+  nav.classList.add('toonMenu');
+});
+
+sluitButton.addEventListener('click', () => {
+  nav.classList.remove('toonMenu');
+});
+
 
